9cc839e35b2bdd1b00d77be9ea5e735674f4c2ae
T3chnGam3r <T3chnGam3r@gmail.com>
2017-12-02 10:40:57 +0000

    Added files

---
+++ LD40/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+	public float MovementSpeed = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Moves the player
+		transform.position = transform.position + (new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * MovementSpeed * Time.deltaTime);
+		
+		// Deals with the camera
+		Camera.main.transform.position = new Vector3(Mathf.Lerp(Camera.main.transform.position.x, transform.position.x, Time.deltaTime), Mathf.Lerp(Camera.main.transform.position.y, transform.position.y, Time.deltaTime), Camera.main.transform.position.z);
+		Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - (Input.GetAxisRaw("Mouse ScrollWheel") * 25f), 5, 25);
+
+	}
+}
---
+++ LD40/Assets/Scripts/Shop.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shop : MonoBehaviour {
+
+	public GameObject ShopPanel;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+	public void ToggleShop()
+	{
+		if (ShopPanel.activeSelf)
+			GetComponentInChildren<Text>().text = "Open Shop";
+		else
+			GetComponentInChildren<Text>().text = "Close Shop";
+		ShopPanel.SetActive(!ShopPanel.activeSelf);
+		
+	}
+}
